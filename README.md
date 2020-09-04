723747ff039892d00bcfd438e288033942e2c526
Oliver Squibb <oliver.squibb@therapyappointment.com>
2020-09-04 19:10:42 -0400

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # udacityfsd_capstone
+
+To run the server, execute:
+```bash
+export FLASK_APP=api.py;
+flask run --reload
+```
---
+++ api.py
@@ -0,0 +1,45 @@
+import os
+from flask import Flask, request, jsonify, abort
+import json
+from flask_cors import CORS
+
+
+app = Flask(__name__)
+CORS(app)
+
+
+## ROUTES
+
+@app.route('/')
+def hello():
+    return jsonify({
+            'success': True,
+            'message': 'hello'
+        }), 200
+
+
+## Error Handling
+
+@app.errorhandler(422)
+def unprocessable(error):
+    return jsonify({
+                    "success": False, 
+                    "error": 422,
+                    "message": "unprocessable"
+                    }), 422
+
+@app.errorhandler(404)
+def unprocessable(error):
+    return jsonify({
+                    "success": False, 
+                    "error": 404,
+                    "message": "resource not found"
+                    }), 404
+
+@app.errorhandler(401)
+def unauthorized(error):
+    return jsonify({
+        "success": False,
+        "error": 401,
+        "message": 'Unathorized'
+    }), 401
